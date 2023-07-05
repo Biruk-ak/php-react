bd00c562f886da1e62d60b917fdda8d12a5316e9
bisrat yared <bisremv@gmail.com>
2023-07-05 13:40:31 +0300

    last

--- backend/src/temp/home.php
+++ backend/src/temp/home.php
@@ -19,9 +19,22 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         // echo $usr["email"];
         exit;
     }
+}if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    if (!isset($_SESSION["user"])) {
+        header("Location: login.html");
+        // echo "Login Succesfull";
+    } else {
+
+        $usr = $_SESSION["user"];
+        echo "<h1>" . $usr["email"] . "</h1><br>";
+        echo "<h1>" . $usr["fname"] . "</h1>";
+        // echo $usr["email"];
+        // echo $usr["email"];
+        exit;
+    }
 }
 
-$usr = Database::get("user", array("email" => $_POST["email"]));
 if (count($usr) != 0) {
     if ($usr["password"] != $_POST["pass1"]) {
         echo "Invalid login credentials";
